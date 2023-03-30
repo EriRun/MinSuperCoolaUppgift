d1781f4e24424434013ff6d139f0740beb5ae0b3
00erirun <00erirun@MS714105>
2023-03-30 15:39:55 +0200

    Added DeployScript

---
+++ DeployScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PublicKey=$(cat ~/.ssh/id_rsa.pub)
+templatePath=~/Documents/CLO22Dev/MinSuperCoolaUppgift/ARM-Template.json
+RG=CoolRG
+deployName=DeployScript
+
+az deployment group create  --name $deployName --resource-group $RG --template-file $templatePath --verbose --parameters adminPublicKey="$PublicKey"
