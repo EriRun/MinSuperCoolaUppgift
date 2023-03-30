534650169c2f81e4c445bd00bd506e81cfc3967f
00erirun <00erirun@MS714105>
2023-03-30 17:34:09 +0200

    added workflow

--- DeployScript.sh
+++ DeployScript.sh
@@ -5,4 +5,4 @@ templatePath=~/Documents/CLO22Dev/MinSuperCoolaUppgift/ARM-Template.json
 RG=CoolRG
 deployName=DeployScript
 
-az deployment group create  --name $deployName --resource-group $RG --template-file $templatePath --verbose --parameters adminPublicKey="$PublicKey"
+az deployment group create --name $deployName --resource-group $RG --template-file $templatePath --verbose --parameters adminPublicKey="$PublicKey"
